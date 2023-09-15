9059379b0f3be457ebf9b9dc1f96437c3ae8cbb9
Pedro-Munoz-Hoyos <112551548+Pedro-Munoz-Hoyos@users.noreply.github.com>
2023-09-16 01:20:04 +0200

    Artificial viscosity function

--- Chapter 8/f_artivis.m
+++ Chapter 8/f_artivis.m
@@ -1,11 +1,10 @@
 function [S] = artivis(Fl,Fc,Fr,rho,Cy)
 % Function in charge of obtaining the artificial viscosity.
 
+S=zeros(4,1);
 spl=Fl(2)-((Fl(1)^2)/rho);
 sp=Fc(2)-((Fc(1)^2)/rho);
 spr=Fr(2)-((Fr(1)^2)/rho);
-
-S=zeros(4,1);
 for i=1:4
     S(i)=Cy*abs(spl-2*sp+spr)*(Fl(i)-2*Fc(i)+Fr(i))/(spl+2*sp+spr);
 end
